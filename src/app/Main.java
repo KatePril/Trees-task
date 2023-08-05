1d690d8f2d7ebd1c748d038e370014e4ee1920d4
KatePril <ket14pril14@gmail.com>
2023-08-05 16:14:21 +0300

    Add nodes

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,13 +2,23 @@ package app;
 
 public class Main {
     public static void main(String[] args) {
-        MyTree tree = new MyTree(5);
+        MyTree tree = new MyTree(15);
+
         System.out.println(tree.addElement(3));
         System.out.println(tree.addElement(7));
         System.out.println(tree.addElement(2));
+
         System.out.println(tree.addElement(4));
         System.out.println(tree.addElement(6));
         System.out.println(tree.addElement(8));
+
+        System.out.println(tree.addElement(20));
+        System.out.println(tree.addElement(17));
+        System.out.println(tree.addElement(25));
+
+        System.out.println(tree.addElement(19));
+        System.out.println(tree.addElement(23));
+        System.out.println(tree.addElement(16));
 
         System.out.println();
         TreeDisplay treeDisplay = new TreeDisplay(tree);
